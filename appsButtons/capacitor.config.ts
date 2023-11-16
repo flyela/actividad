fb76799fd650b09c28f726115225b4259340a498
Flyela <flyela@estudiante.uniajc.edu.co>
2023-11-15 22:05:33 -0500

    moviles c3

---
+++ appsButtons/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appsButtons',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
