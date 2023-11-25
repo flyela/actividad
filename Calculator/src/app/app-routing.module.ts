08ed6ef48c669062cb913095cf3ca43cfa8e1ca9
Flyela <flyela@estudiante.uniajc.edu.co>
2023-11-25 10:55:48 -0500

    Nuevos Cambios

--- Calculator/src/app/app-routing.module.ts
+++ Calculator/src/app/app-routing.module.ts
@@ -19,4 +19,12 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+
+
+
+
+  
+ }
--- Calculator/src/app/home/home.page.ts
+++ Calculator/src/app/home/home.page.ts
@@ -6,31 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // La variable 'display' almacena la expresión que se mostrará en la calculadora.
   display: string = '';
+
+  // El arreglo 'buttons' contiene los botones que se mostrarán en la calculadora.
   buttons: string[] = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+', 'C'];
 
+  // Maneja los clics en los botones de la calculadora.
   onButtonClick(button: string) {
     if (button === '=') {
+      // Si se hace clic en '=', realiza el cálculo.
       this.calculate();
     } else if (button === 'C') {
+      // Si se hace clic en 'C', borra la pantalla.
       this.clearDisplay();
     } else {
+      // Para otros botones, simplemente agrega el valor al 'display'.
       this.display += button;
     }
   }
 
+  // Realiza el cálculo de la expresión actual y actualiza la pantalla.
   calculate() {
     try {
       this.display = eval(this.display);
     } catch (error) {
+      // Maneja errores durante la evaluación de la expresión.
       this.display = 'Error';
     }
   }
 
+  // Borra la pantalla.
   clearDisplay() {
     this.display = '';
   }
 
+  // Asigna una clase CSS específica al botón '=' para estilizarlo de manera diferente.
   buttonClass(button: string): string {
     return button === '=' ? 'operator' : '';
   }
